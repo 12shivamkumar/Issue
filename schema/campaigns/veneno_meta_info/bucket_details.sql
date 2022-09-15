264cdbe9980f1aa384e5f0c4582402ba934b0c18
shivamGupta <shivamguptaniki@gmail.com>
2022-09-15 14:02:04 +0530

    Initial commit

---
+++ schema/campaigns/veneno_meta_info/bucket_details.sql
@@ -0,0 +1,13 @@
+
+
+CREATE TABLE `bucket_details` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `shard_id` int(11) NOT NULL,
+  `bucket_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `hit_rate` int(11) NOT NULL,
+  `rows_count` int(11) DEFAULT NULL,
+  `last_visited_on` date NOT NULL,
+  `has_sub_delivery_status` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) 
+
